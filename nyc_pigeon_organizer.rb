8f27a521677a89939d38d58ed4716b20aa089d7e
chaplonglau <chaplonglau@gmail.com>
2016-06-07 12:01:47 -0400

    FIRST TRY

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,41 @@
+
+require 'pry'
+pigeon_data = {
+  :color => {
+    :purple => ["Theo", "Peter Jr.", "Lucky"],
+    :grey => ["Theo", "Peter Jr.", "Ms. K"],
+    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+    :brown => ["Queenie", "Alex"]
+  },
+  :gender => {
+    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+    :female => ["Queenie", "Ms. K"]
+  },
+  :lives => {
+    "Subway" => ["Theo", "Queenie"],
+    "Central Park" => ["Alex", "Ms. K", "Lucky"],
+    "Library" => ["Peter Jr."],
+    "City Hall" => ["Andrew"]
+  }
+}
+
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+
+  data.each do |properties, dataset|
+    dataset.each do |x, namearray|
+      namearray.each do |name|
+        if (pigeon_list.key?(name)==false)
+            pigeon_list[name]= {:color => [],
+                                 :gender => [],
+                                 :lives => [] }
+        end
+        pigeon_list[name][properties]<<x.to_s
+      end
+    end
+  end
+  pigeon_list
 end
+
+
